25ae482d11b86c8ba39345da929b8440a5d1c960
Duretitibesso3 <gdureti@gmail.com>
2022-05-18 08:12:51 -0500

    Add script,js file

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('#bar');
+const close = document.querySelector('#close');
+const navigation = document.querySelector('.barlist');
+const textPortfolio = document.querySelector('.nav-link-1');
+const about = document.querySelector('.nav-link-2');
+const contact = document.querySelector('.nav-link-3');
