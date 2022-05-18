73cc6328ba8da83f341c0b212f407c887934950b
Duretitibesso3 <gdureti@gmail.com>
2022-05-18 08:31:24 -0500

    Edit script.js file

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navigation = document.querySelector('.barlist');
 const textPortfolio = document.querySelector('.nav-link-1');
 const about = document.querySelector('.nav-link-2');
 const contact = document.querySelector('.nav-link-3');
+
+hamburger.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  navigation.classList.toggle('show');
+  hamburger.classList.add('hide');
+  close.classList.remove('hide');
+  textPortfolio.classList.add('menu-text');
+  about.classList.add('menu-text');
+  contact.classList.add('menu-text');
+});
